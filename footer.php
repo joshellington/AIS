7e0bfb00206881488d5a13f2bb3afb9f7016199f
Joshua Ellington <hello@joshellington.com>
2016-12-05 11:18:31 -0800

    Added GA tracking

--- footer.php
+++ footer.php
@@ -5,6 +5,16 @@
     </div> <!-- end .container -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/mousetrap/1.4.6/mousetrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js"></script>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-88457454-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
     <?php wp_footer(); ?>
   </body>
 </html>
